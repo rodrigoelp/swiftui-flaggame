c01b371e4cbfc8c4b0bc41eb168351c38a1dc6cc
Rodrigo Landaeta <rlandaeta@cochlear.com>
2019-08-08 10:40:23 +1000

    Fixed a few problems with the flags and wired up a few controls to check everything is -sort of- working.

--- Flag Game/ContentView.swift
+++ Flag Game/ContentView.swift
@@ -9,12 +9,47 @@ struct ContentView: View {
         .filter({ $0.offset < 3 })
         .map({ $0.element })
     
+    var answerIndex = Int.random(in: 0...2)
+    
+    @State private var score = 0
+    @State private var showAlert = false
+    @State private var alertTitle = ""
+    
     var body: some View {
-        VStack {
-            ForEach(countries, id: \Country.code) { country in
-                Text(country.name)
+        NavigationView {
+                VStack(alignment: .center, spacing: 8) {
+                    Text("Which flag belongs to: \(countries[answerIndex].name)")
+                    ForEach(countries, id: \Country.code) { country in
+                        country.image
+                            .border(Color.black, width: 2)
+                            .onTapGesture {
+                                self.flagTapped(country)
+                        }
+                    }
             }
+                .navigationBarTitle(Text("Score: \($score.value)"))
+                .alert(isPresented: $showAlert) {
+                    Alert(title: Text($alertTitle.value), dismissButton: .default(Text("Continue")))
+                }
         }
+    }
+    
+    func flagTapped(_ country: Country) {
+        let answerCountry = countries[answerIndex]
+        if answerCountry.code == country.code {
+            score += 1
+            alertTitle = "You are right!"
+        } else {
+            score -= 1
+            alertTitle = "Close but not cigar 😕"
+        }
+        showAlert = true
+    }
+}
+
+extension Country {
+    var image: Image {
+        Image(self.imageName)
     }
 }
 
